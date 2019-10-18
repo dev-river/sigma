ecde0082f8de9976120187952897b343da7e854e
leejammok <woanr263@naver.com>
2019-10-18 17:23:31 +0900

    판매자 페이지 수정 중 1

--- Sigma/src/main/java/kr/co/controller/compController.java
+++ Sigma/src/main/java/kr/co/controller/compController.java
@@ -1,6 +1,8 @@
 package kr.co.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -9,7 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.co.domain.gameDetailDcVO;
 import kr.co.domain.gameVO;
 import kr.co.domain.memberVO;
 import kr.co.domain.refundVO;
@@ -33,6 +37,7 @@ public class compController {
 	@RequestMapping(value = "/compInform/update")
 	public void updateUI(@RequestParam String id, Model model) {
 		memberVO vo = compservice.read(id);
+		System.out.println(vo.getId());
 		model.addAttribute("vo", vo);
 	}
 	
@@ -49,8 +54,10 @@ public class compController {
 	
 	@RequestMapping(value = "/gameList/gameList")
 	public void gamelist(String writer, Model model) {
-		List<gameVO> list = compservice.gamelist(writer);
-		model.addAttribute("gamelist", list);
+		List<gameVO> gamelist = compservice.gamelist(writer);
+		List<gameDetailDcVO> gameDetailDC =  compservice.gameDetailDC(writer);
+		model.addAttribute("gamelist", gamelist);
+		model.addAttribute("gameDetailDC", gameDetailDC);
 	}
 	
 	@RequestMapping(value = "/gameList/gameInsert")
@@ -59,13 +66,39 @@ public class compController {
 	@RequestMapping(value = "/gameList/gameInsert", method = RequestMethod.POST)
 	public String gameinsert(gameVO vo) {
 		compservice.gameinsert(vo);
-		return "redirect:/compManage/gameList/gameList?writer=idid";
+		return "redirect:/compManage/gameList/gameList?writer="+vo.getWriter();
 	}
 	
 	@RequestMapping(value = "/refund/refundList")
 	public void refundList(@RequestParam String id, Model model) {
 		List<refundVO> list = compservice.refundList(id);
-		System.out.println("list : "+list);
-		model.addAttribute("refundList", list);
+		model.addAttribute("list", list);
 	}
+	
+	@RequestMapping(value = "/refund/refundRead")
+	public void refundRead(String id, Model model) {
+		refundVO rvo = compservice.refundRead(id);
+		model.addAttribute("vo", rvo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/refund/refundOK", method = RequestMethod.POST)
+	public void refundOK(int num, int buynum, String id) {
+		compservice.refundOK(num);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("buynum", buynum);
+		map.put("id", id);
+		compservice.returncash(map);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/refund/refundReject", method = RequestMethod.POST)
+	public void refundReject(String id, int num, String inputString) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("num", num);
+		map.put("inputString", inputString);
+		compservice.refundReject(map);
+	}
+	
+	
 }
